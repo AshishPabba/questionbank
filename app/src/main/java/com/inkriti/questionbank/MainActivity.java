b46edbfd7d46b4c50abca6bf5253afa067e427ab
Ashish Pabba <ashish.pabba@gmail.com>
2016-07-18 18:16:12 +0530

    working on navigation drawer

--- app/src/main/java/com/inkriti/questionbank/MainActivity.java
+++ app/src/main/java/com/inkriti/questionbank/MainActivity.java
@@ -1,15 +1,32 @@
 package com.inkriti.questionbank;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.GridView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] mDrawerOptions;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//
+//        mDrawerOptions = getResources().getStringArray(R.array.MenuOptions);
+//        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+//
+//        // Set the adapter for the list view
+//        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+//                R.layout.drawer_list_item, mDrawerOptions));
+//        // Set the list's click listener
+//        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
 
         GridView boardsGridview = (GridView) findViewById(R.id.boardsGridView);
         boardsGridview.setAdapter(new BoardAdapter(this));
